b296ee3780b10e9bf69d61d7eba8e36b5fa7da24
Silvano Amaral <silvano.amaral@pmweb.com.br>
2017-12-07 10:47:40 -0200

    Criando modulo cursos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
+/* import das nossas classes */
 import { AppComponent } from './app.component';
 import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
+import { CursosModule } from './cursos/cursos.module';
 
 @NgModule({
   declarations: [
@@ -13,7 +15,8 @@ import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
     MeuPrimeiro2Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CursosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
